45f6450bb68d015408b1b3104d89d595abc63dd1
ruthpaulsson <ruth.paulsson@gmail.com>
2026-04-28 15:58:38 +0200

    Initial commit

---
+++ habbitz/habbitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  habbitzApp.swift
+//  habbitz
+//
+//  Created by Ruth Paulsson on 2026-04-28.
+//
+
+import SwiftUI
+
+@main
+struct habbitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
